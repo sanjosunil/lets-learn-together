b5dda54b196e3cf606e552b70da56f115df8dbe3
Indru922 <indrajithmv2006@gmail.com>
2024-11-13 21:10:59 +0530

    Added Indru's quiz

---
+++ activities/Indrajith/quiz.py
@@ -0,0 +1,18 @@
+input("starting position of an array is ? \n 1)1 \n 2)0 \n 3)-1")
+ans1=str(input(" enter the option"))
+if ans1=="2":
+    print("right answer")
+else:
+    print("wrong answer ")
+input("Git is a \n 1)website \n 2)version control system(VCS)" )
+ans2=str(input("enter the option"))
+if ans2=="2" :
+    print("right answer")
+else:
+    print("wrong answer")
+input("Do you need github to use git? \n 1)yes \n 2)no")
+ans3=str(input("enter the option"))
+if ans3=="2":
+    print("right answer")
+else:
+    print("wrong answer")
